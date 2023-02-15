7b50d5bd659997f1aa092f0dab3883ab821290f0
Gustavo Vilela <gutusmao12@hotmail.com>
2023-02-15 19:58:05 -0300

    Criando e implementando cadastro de tarefas

--- gerenciador-de-tarefas/src/app/tarefas/tarefas.module.ts
+++ gerenciador-de-tarefas/src/app/tarefas/tarefas.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 
 import { TarefaService } from './shared';
 import { ListarTarefaComponent } from './listar';
+import { CadastrarTarefaComponent } from './cadastrar';
 
 
 
 @NgModule({
   declarations: [
-    ListarTarefaComponent
+    ListarTarefaComponent,
+    CadastrarTarefaComponent
   ],
   imports: [
     CommonModule,
