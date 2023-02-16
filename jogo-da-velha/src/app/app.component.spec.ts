023b4d3b69f3feb6e26c0da26b71daa7e4d7815d
Gustavo Vilela <gutusmao12@hotmail.com>
2023-02-16 16:26:28 -0300

    Criando os primeiros componentes

--- jogo-da-velha/src/app/app.component.spec.ts
+++ jogo-da-velha/src/app/app.component.spec.ts
@@ -1,11 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { JogoDaVelhaModule } from './jogo-da-velha/jogo-da-velha.module';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
         AppComponent
+      ],
+      imports: [
+        JogoDaVelhaModule
       ],
     }).compileComponents();
   });
